a311c6c7e8197f948042a43a5e8c6f0f8565bd71
Dubier_Barrientos <129345580+Dubier-Barrientos@users.noreply.github.com>
2023-10-09 23:28:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,17 @@ import time
 import glob
 from textblob import TextBlob
 import pandas as pd
+import cv2
+import numpy as np
+import pytesseract
 
 from gtts import gTTS
 from googletrans import Translator
 
-selected_page = st.sidebar.radio("Selecciona una opción:", ["Multilenguaje", "Cámara"])
+selected_page = st.sidebar.radio("Selecciona una opción:", ["Te escuchamos", "Cámara"])
 
-if selected_page == "Multilenguaje":
-    st.title("Escucha tu voz en otros idiomas")
-    st.subheader("Toca el Botón y habla lo que quires traducir")
+if selected_page == "Te escuchamos":
+    st.subheader("¿Que frase quieres decirnos?")
     
     
     #image = Image.open('traductor.jpg')
@@ -59,73 +61,6 @@ if selected_page == "Multilenguaje":
         except:
             pass
 
-        translator = Translator()
-        
-        text = str(result.get("GET_TEXT"))
-        in_lang = st.selectbox(
-            "Selecciona el lenguaje de Entrada",
-            ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés", "Italiano"),
-        )
-        if in_lang == "Inglés":
-            input_language = "en"
-        elif in_lang == "Español":
-            input_language = "es"
-        elif in_lang == "Bengali":
-            input_language = "bn"
-        elif in_lang == "Coreano":
-            input_language = "ko"
-        elif in_lang == "Mandarín":
-            input_language = "zh-cn"
-        elif in_lang == "Japonés":
-            input_language = "ja"
-        elif in_lang == "Italiano":
-            input_language = "it"   
-        
-        out_lang = st.selectbox(
-            "Selecciona el lenguaje de salida",
-            ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés", "Italiano"),
-        )
-        if out_lang == "Inglés":
-            output_language = "en"
-        elif out_lang == "Español":
-            output_language = "es"
-        elif out_lang == "Bengali":
-            output_language = "bn"
-        elif out_lang == "Coreano":
-            output_language = "ko"
-        elif out_lang == "Mandarín":
-            output_language = "zh-cn"
-        elif out_lang == "Japonés":
-            output_language = "ja"
-        elif out_lang == "Italiano":
-            output_language = "it"   
-        
-        def text_to_speech(input_language, output_language, text, tld):
-            translation = translator.translate(text, src=input_language, dest=output_language)
-            trans_text = translation.text
-            tts = gTTS(trans_text, lang=output_language, tld=tld, slow=False)
-            try:
-                my_file_name = text[0:20]
-            except:
-                my_file_name = "audio"
-            tts.save(f"temp/{my_file_name}.mp3")
-            return my_file_name, trans_text
-        
-        
-        display_output_text = st.checkbox("Mostrar el texto")
-        
-        if st.button("Traduce tu voz"):
-            result, output_text = text_to_speech(input_language, output_language, text, tld)
-            audio_file = open(f"temp/{result}.mp3", "rb")
-            audio_bytes = audio_file.read()
-            st.markdown(f"## Tú audio:")
-            st.audio(audio_bytes, format="audio/mp3", start_time=0)
-        
-            if display_output_text:
-                st.markdown(f"#### Texto de salida:")
-                st.write(f" {output_text}")
-        
-        
         def remove_files(n):
             mp3_files = glob.glob("temp/*mp3")
             if len(mp3_files) != 0:
@@ -138,7 +73,7 @@ if selected_page == "Multilenguaje":
     
         remove_files(7)
 
-        st.header('También puedes hacer un análisis de sentimiento')
+        st.header('¿Quieres hacer un análisis de sentimiento?')
         if st.button("Analizar"):
             if text:
                 translation = translator.translate(text, src="es", dest="en")
@@ -155,5 +90,26 @@ if selected_page == "Multilenguaje":
                     st.write( 'Es un sentimiento Neutral 😐')
 
 elif selected_page == "Cámara":
-    st.write("hello")
+    st.title("Reconocimiento óptico de Caracteres")
+
+    img_file_buffer = st.camera_input("Toma una Foto")
+    
+    with st.sidebar:
+          filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
+    
+    
+    if img_file_buffer is not None:
+        # To read image file buffer with OpenCV:
+        bytes_data = img_file_buffer.getvalue()
+        cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+        
+        if filtro == 'Con Filtro':
+             cv2_img=cv2.bitwise_not(cv2_img)
+        else:
+             cv2_img= cv2_img
+        
+            
+        img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+        text=pytesseract.image_to_string(img_rgb)
+        st.write(text)
     
